2f9935ff1f6371df3fbaa4263453ee7cdcc56a0f
Badr Ben m'barek <badr.benb@gmail.com>
2024-01-16 12:25:59 +0000

    feat: add requestbuilder for enstoe

--- power_stash/inputs/entsoe/request.py
+++ power_stash/inputs/entsoe/request.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from enum import Enum
 from typing import Literal
 
 from entsoe.mappings import Area
 from pydantic import Field
 
-from power_stash.models.request import BaseRequest
+from power_stash.models.request import BaseRequest, BaseRequestBuilder
 
 
 class RequestType(str, Enum):
@@ -26,12 +27,40 @@ class EntsoeRequest(BaseRequest):
     request_type: RequestType = Field(description="type of data requested.")
     net_number: bool | None = Field(
         default=None,
-        description="When applicable, condense generation and consumption into a net number",
+        description="""
+        Condense generation and consumption into a net number.
+        Valid when `request_type=="generation"`.
+        """,
     )
     resolution: Literal["60T", "30T", "15T"] | None = Field(
         default=None,
         description="""
-        When applicable, either 60T for hourly values, 30T for half-hourly values
-        or 15T for quarterly values
+        Either 60T for hourly values, 30T for half-hourly values or 15T for quarterly values.
+        Valid when request_type="day-ahead price"
         """,
     )
+
+
+class EntsoeRequestBuilder(BaseRequestBuilder):
+    def __init__(self) -> None:
+        self.areas = list(Area)
+        self.request_types = list(RequestType)
+
+    def build_default_requests(self, start: datetime, end: datetime) -> list[EntsoeRequest]:
+        """Return all the default requests for the given dates.
+
+        Download all areas as defined in enum Area,
+        for all request_types defined in enum RequestType.
+        """
+        all_requests = []
+        for area in self.areas:
+            for request_type in self.request_types:
+                new_request = EntsoeRequest(
+                    start=start,
+                    end=end,
+                    area=area,
+                    request_type=request_type,
+                )
+                all_requests.append(new_request)
+
+        return all_requests
